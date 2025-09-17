b4bc37ddb24c11625c0adb16740315e67b4af095
Florian Friedrich <florian.friedrich@sersoft.de>
2025-09-17 13:09:03 +0200

    Remove unneeded compiler directive

--- Sources/AppGroups/AppGroup.swift
+++ Sources/AppGroups/AppGroup.swift
@@ -66,11 +66,10 @@ extension AppGroup {
 
 fileprivate extension URL {
     func _appendingDirectory(_ dirName: String) -> URL {
-#if swift(>=6.0) || canImport(Darwin)
         if #available(macOS 13, iOS 16, tvOS 16, watchOS 9, *) {
             return appending(component: dirName, directoryHint: .isDirectory)
+        } else {
+            return appendingPathComponent(dirName, isDirectory: true)
         }
-#endif
-        return appendingPathComponent(dirName, isDirectory: true)
     }
 }
